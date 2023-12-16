10108632072bbc7ec804e9e2d06d23795de3fa36
linenive <linenive@naver.com>
2023-12-17 03:42:48 +0900

    커스텀 렌더링 파이프라인과 RP애셋 생성
    customRP 위치 변경

---
+++ Assets/CustomRP/Runtime/CameraRenderer.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRenderer
+{
+    ScriptableRenderContext context;
+    
+    Camera camera;
+
+    public void Render (ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+    }
+}
---
+++ Assets/CustomRP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipeline : RenderPipeline
+{
+    CameraRenderer renderer = new CameraRenderer();
+
+    // Unity 2022 이전에 사용하던 함수이지만 abstract로 선언되어 있으므로 유지해 둔다.
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras) { }
+
+    protected override void Render(ScriptableRenderContext context, List<Camera> cameras)
+    {
+        foreach (var camera in cameras)
+        {
+            renderer.Render(context, camera);
+        }
+    }
+}
