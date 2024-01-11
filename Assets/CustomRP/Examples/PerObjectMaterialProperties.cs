ef6b18c33cfc9624c88afd22dc4a5f1572a66b72
linenive <linenive@naver.com>
2024-01-11 21:43:01 +0900

    metallic, smoothness 속성 추가
    Update PerObjectMaterialProperties.cs
    
    Update SampleScene.unity

--- Assets/CustomRP/Examples/PerObjectMaterialProperties.cs
+++ Assets/CustomRP/Examples/PerObjectMaterialProperties.cs
@@ -5,11 +5,13 @@ public class PerObjectMaterialProperties : MonoBehaviour
 {
     private static int baseColorId = Shader.PropertyToID("_BaseColor");
     private static int cutoffId = Shader.PropertyToID("_Cutoff");
+    private static int metalicId = Shader.PropertyToID("_Metallic");
+    private static int smoothnessId = Shader.PropertyToID("_Smoothness");
 
     [SerializeField]
     private Color baseColor = Color.white;
     [SerializeField, Range(0f, 1f)]
-    private float cutoff = 0.5f;
+    private float cutoff = 0.5f, metallic = 0f, smoothness = 0.5f;
     private static MaterialPropertyBlock block;
 
     private void Awake()
@@ -25,6 +27,8 @@ public class PerObjectMaterialProperties : MonoBehaviour
         }
         block.SetColor(baseColorId, this.baseColor);
         block.SetFloat(cutoffId, cutoff);
+        block.SetFloat(metalicId, metallic);
+        block.SetFloat(smoothnessId, smoothness);
         this.GetComponent<Renderer>().SetPropertyBlock(block);
     }
 }
