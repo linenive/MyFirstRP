f1b572279b2c327effefd88a9cf76910ae1be166
linenive <linenive@naver.com>
2024-01-11 22:05:45 +0900

    Specular 색상, 거칠기 계산 추가
    Update Common.hlsl

--- Assets/CustomRP/ShaderLibrary/BRDF.hlsl
+++ Assets/CustomRP/ShaderLibrary/BRDF.hlsl
@@ -17,8 +17,11 @@ BRDF GetBRDF (Surface surface) {
 	BRDF brdf;
 
 	brdf.diffuse = surface.color * OneMinusReflectivity(surface.metallic);
-	brdf.specular = 0.0;
-	brdf.roughness = 1.0;
+    // 나가는 빛의 양이 들어오는 빛의 양을 초과할 수 없으므로.
+	brdf.specular = lerp(MIN_REFLECTIVITY, surface.color, surface.metallic);
+	float perceptualRoughness =
+		PerceptualSmoothnessToPerceptualRoughness(surface.smoothness);
+	brdf.roughness = PerceptualRoughnessToRoughness(perceptualRoughness);
 	return brdf;
 }
 
