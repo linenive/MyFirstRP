07693293c91d44de8b76dbf598449af15cff8bc6
linenive <linenive@naver.com>
2024-01-11 22:32:40 +0900

    메시 볼

--- Assets/CustomRP/Examples/MeshBall.cs
+++ Assets/CustomRP/Examples/MeshBall.cs
@@ -3,14 +3,19 @@ using UnityEngine;
 public class MeshBall : MonoBehaviour
 {
     private static int baseColorId = Shader.PropertyToID("_BaseColor");
+    private static int metallicId = Shader.PropertyToID("_Metallic");
+    private static int smoothnessId = Shader.PropertyToID("_Smoothness");
 
     [SerializeField]
     private Mesh mesh = default;
 
     [SerializeField]
     private Material material = default;
+
     private Matrix4x4[] matrices = new Matrix4x4[1023];
     private Vector4[] baseColors = new Vector4[1023];
+    private float[] metallic = new float[1023];
+    private float[] smoothness = new float[1023];
     private MaterialPropertyBlock block;
 
     private void Awake()
@@ -25,6 +30,8 @@ public class MeshBall : MonoBehaviour
             this.baseColors[i] =
               new Vector4(Random.value, Random.value, Random.value,
               Random.Range(0.5f, 1f));
+            metallic[i] = Random.value < 0.25f ? 1f : 0f;
+            smoothness[i] = Random.Range(0.05f, 0.95f);
         }
     }
 
@@ -34,6 +41,8 @@ public class MeshBall : MonoBehaviour
         {
             this.block = new MaterialPropertyBlock();
             this.block.SetVectorArray(baseColorId, this.baseColors);
+            block.SetFloatArray(metallicId, this.metallic);
+            block.SetFloatArray(smoothnessId, this.smoothness);
         }
         Graphics.DrawMeshInstanced(this.mesh, 0, this.material, this.matrices, 1023, this.block);
     }
