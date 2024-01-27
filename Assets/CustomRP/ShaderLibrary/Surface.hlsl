6e15e7c56b9e736da6dae5f0ec0665f705992375
linenive <linenive@naver.com>
2024-01-27 20:45:59 +0900

    샘플링 섀도

--- Assets/CustomRP/ShaderLibrary/Surface.hlsl
+++ Assets/CustomRP/ShaderLibrary/Surface.hlsl
@@ -2,6 +2,7 @@
 #define CUSTOM_SURFACE_INCLUDED
 
 struct Surface {
+    float3 position;
     float3 normal; // 조명 계산이 어디에서 일어나는 지 모르기 떄문에 normalWS를 사용하지 않음
     float3 viewDirection;
     float3 color;
